e4aee8035fe1f9f2724a20a724644ad4ccb7177f
Supun Rashmika <supunrworks@gmail.com>
2026-08-08 21:07:15 +0530

    Added dark mode toggle option

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,5 +1,8 @@
+import { ThemeProvider } from "@/components/theme-provider"
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/ui/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +22,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="en" suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar/>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ frontend/components/ui/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ModeToggle } from '../mode-toggle'
+
+function Navbar() {
+  return (
+    <div>
+      <ModeToggle/>
+    </div>
+  )
+}
+
+export default Navbar
